84a417c2913cb6c192758673d2ab112294063914
blinkins0n <tarranet@gmail.com>
2021-09-21 16:39:19 +0100

    Goroutines and WaitGroup use

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"sync"
 	"time"
 
 	"github.com/webdeveloppro/golang-websocket-client/pkg/client"
 )
 
+var N_MESSAGES = 5
+var N_CLIENTS = 5
+
 var addr = flag.String("addr", ":8000", "http service address")
 
-func main() {
-	flag.Parse()
+func worker(id int) {
 
 	client, err := client.NewWebSocketClient(*addr, "frontend")
 	if err != nil {
@@ -23,28 +23,40 @@ func main() {
 
 	fmt.Println("Connecting")
 
-	go func() {
-		// write down data every 100 ms
-		ticker := time.NewTicker(time.Millisecond * 1500)
-		i := 0
-		for range ticker.C {
-			err := client.Write(i)
-			if err != nil {
-				fmt.Printf("error: %v, writing error\n", err)
-			}
-			i++
+	// write down data every 100 ms
+	for i := 0; i < N_MESSAGES; i++ {
+		err := client.Write(i)
+		if err != nil {
+			fmt.Printf("error: %v, writing error\n", err)
 		}
-	}()
 
-	// Close connection correctly on exit
-	sigs := make(chan os.Signal, 1)
+		// Sleep to simulate an expensive task.
+		time.Sleep(time.Second)
+	}
 
-	// `signal.Notify` registers the given channel to
-	// receive notifications of the specified signals.
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	// The program will wait here until it gets the
-	<-sigs
 	client.Stop()
+
+	time.Sleep(time.Second)
+}
+
+func main() {
+
+	flag.Parse()
+
+	var wg sync.WaitGroup
+
+	for i := 1; i <= N_CLIENTS; i++ {
+		wg.Add(1)
+
+		i := i
+
+		go func() {
+			defer wg.Done()
+			worker(i)
+		}()
+	}
+
+	wg.Wait()
+
 	fmt.Println("Goodbye")
 }
